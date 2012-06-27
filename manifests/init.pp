0af92b657110b805241caa57a00a373d442d4661
dazz <annejulia@gmail.com>
2012-06-27 22:04:18 +0200

    debugged composer module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,45 +1,48 @@
-class composer($targetdir) {
+class composer($target_dir="/usr/local/bin", $composer_file = "composer") {
 
-    include composer::params
+  include augeas
+  include composer::params
 
-    # install wget
-    package {"wget":
-        ensure => present,
-    }
+  package {["curl", "php5-cli"]:
+    ensure => present,
+  }
 
-    file {"$targetdir":
-        ensure => present
-    }
+  $download_url = "http://getcomposer.org/installer"
+  $tmp_path     = "/home/vagrant"
 
-    package {"php5-cli":
-        ensure => present
-    }
+  # download composer
+  exec { "download_composer":
+    command     => "curl -s $download_url | php",
+    logoutput   => true,
+    cwd         => $tmp_path,
+    require     => Package["curl", "php5-cli"],
+    creates     => "$tmp_path/composer.phar",
+  }
 
-    # trage werte in die php.ini ein
-    # set /etc/php5/cli/php.ini/suhosin.executor.include.whitelist = phar
-    augeas{"whitelist_phar" :
-        context => "/files/etc/php5/conf.d/suhosin.ini/suhosin",
-        changes => "set suhosin.executor.include.whitelist phar",
-        require => [
-            Package["php5-cli"],
-        ],
-    }
-  
-    augeas{"allow_url_fopen" :
-        context => "/files/etc/php5/cli/php.ini/PHP",
-        changes => "set allow_url_fopen On",
-        require => [
-            Package["php5-cli"],
-        ],
-    }
-   
-    # download composer
-    exec { "download_composer":
-        command => "wget http://getcomposer.org/installer -O composer.phar",
-        require => [Package["wget"], File["$targetdir"]],
-        logoutput => true,
-        creates => "$targetdir/composer.phar",
-        cwd => "$targetdir",
-        user => root
-    }
+  # move file
+  file { "$target_dir/$composer_file":
+    ensure      => present,
+    source      => "$tmp_path/composer.phar",
+    require     => Exec["download_composer"],
+    group       => "staff",
+    mode        => 0775,
+  }
+
+  exec { "update_composer":
+    command     => "$target_dir/$composer_file self-update",
+    require     => File["$target_dir/$composer_file"],
+  }
+
+  # set /etc/php5/cli/php.ini/suhosin.executor.include.whitelist = phar
+  augeas { "whitelist_phar":
+    context     => "/files/etc/php5/conf.d/suhosin.ini/suhosin",
+    changes     => "set suhosin.executor.include.whitelist phar",
+    require     => Class["augeas"],
+  }
+
+  augeas{ "allow_url_fopen":
+    context     => "/files/etc/php5/cli/php.ini/PHP",
+    changes     => "set allow_url_fopen On",
+    require     => Package["php5-cli"],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,32 +1,3 @@
 class composer::params {
-	
-    # 'wget' or 'curl'
-    $download_method = $download_method ? {
-        "" => "wget",
-        default => $download_method	
-    }
 
-    $download_url = $download_url ? {
-        "" => "http://getcomposer.org/installer",
-        default => $download_url,
-    }
-
-    $target_dir = $target_dir ? {
-        "" => "/usr/local/src",
-        default => $target_dir,
-    }
-
-    $exec_download = $exec_download ? {
-        "" => $download_method ? {
-	    'wget'  => 'wget http://getcomposer.org/composer.phar',
-	    'curl'  => 'curl -s http://getcomposer.org/installer | php',
-            default => "curl -s http://getcomposer.org/installer | php"
-        },
-        default => $exec_download
-    }
-
-    $composer_path = $composer_path ? {
-        "" => "${target_dir}/composer.phar",
-        default => $composer_path
-    }  
-} 
+}
